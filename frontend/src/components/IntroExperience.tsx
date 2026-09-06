2bca5e65237938aa8180a0d261850f243419c08f
SKYGOD07 <sahilsharma361004@gmail.com>
2026-09-06 23:29:56 +0530

    updates version 3

--- frontend/src/components/IntroExperience.tsx
+++ frontend/src/components/IntroExperience.tsx
@@ -138,7 +138,6 @@ export function IntroExperience({ open, onClose }: Props) {
   }, [open]);
 
   const close = useCallback(() => {
-    console.trace('[intro] close() called');
     setClosing(true);
     try { localStorage.setItem(SEEN_KEY, '1'); } catch { /* ignore */ }
     // Let the exit animation finish before unmounting. 320ms matches
